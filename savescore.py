425e106899c6610b91787081e0de97cd93b85d8a
Victor Freitas <97434584+victorphreitas@users.noreply.github.com>
2022-10-23 17:24:07 -0300

    Delete savescore.py

--- savescore.py
+++
@@ -1,8 +0,0 @@
-from turtle import Turtle
-from scoreboard import Scoreboard
-
-class Savescore(Turtle):
-
-    def __init__(self):
-        current_score = Scoreboard()
-        self.total = current_score.score
